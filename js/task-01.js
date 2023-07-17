655931dc582fe2c89b5a8a604f6f6503a0c68500
DevMasterX <sokol080288@gmail.com>
2023-07-17 14:17:13 +0300

    update 01 js

--- js/task-01.js
+++ js/task-01.js
@@ -1,20 +1,8 @@
-const categoriesList = document.querySelector("#categories");
-const categoryItems = categoriesList.querySelectorAll("li.item");
 
-const category = document.querySelectorAll(".item h2");
+const itemsList = document.querySelectorAll('.item');
+console.log(`Number of categories: ${itemsList.length}`);
 
-let categoryCount = 0;
-
-categoryItems.forEach(() => {
-  categoryCount += 1;
-});
-
-console.log(`Number of categories: ${categoryCount}`);
-
-categoryItems.forEach((categoryItem) => {
-  const categoryTitle = categoryItem.querySelector("h2").textContent;
-  const categoryElements = categoryItem.querySelectorAll("li").length;
-
-  console.log(`Category: ${categoryTitle}`);
-  console.log(`Elements: ${categoryElements}`);
+itemsList.forEach((item) => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`)
 });
